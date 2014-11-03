3cda65f4043e4a2d523f9de1716ca26219449700
Callum Macrae <callum@macr.ae>
2014-11-03 11:20:45 +0000

    Generate letter HTML

--- assets/js/monkey/steps/letters/generateHtml.js
+++ assets/js/monkey/steps/letters/generateHtml.js
@@ -2,8 +2,34 @@
 
 module.exports = function () {
   return function (data) {
-    data.letters = data.html.parents('[data-key="lmn-book"]')
-      .find('[data-key="monkey-letters"]');
+    var $lettersContainer = $('<div />');
+
+    $lettersContainer.attr({
+      id: 'letters-container',
+      'class': 'aligned-center row leaded',
+      'data-key': 'monkey-letters'
+    });
+
+    $('<p />').appendTo($lettersContainer)
+      .addClass('unleaded')
+      .text('A personalised book made for'); // @todo: Localise
+
+    var $letters = $('<span />').appendTo($lettersContainer)
+      .addClass('strong')
+      .attr('id', 'letters');
+
+    $.each(data.name.split(''), function (i, letter) {
+      $('<span />').appendTo($letters).text(letter).after(' ');
+    });
+
+    $('<span />').html('&bull;')
+      .prependTo($letters)
+      .after(' ')
+      .clone().appendTo($letters);
+
+    var $book = data.html.parents('[data-key="lmn-book"]');
+    data.letters = $lettersContainer.prependTo($book);
+
     return data;
   };
 };
