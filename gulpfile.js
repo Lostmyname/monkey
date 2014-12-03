815a2220f984b9131e4f8b86e011d4437fd28961
Callum Macrae <callum@macr.ae>
2014-12-03 11:34:16 +0000

    Improved gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,24 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var stylish = require('jshint-stylish');
 
+var dieOnError = true;
+
 gulp.task('js-quality', function () {
-  return gulp.src('js/**/*.js')
-    .pipe(plugins.jscs())
-    .pipe(plugins.jshint.reporter(stylish))
-    .pipe(plugins.jshint.reporter('fail'));
+  var stream = gulp.src('js/**/*.js');
+
+  if (!dieOnError) {
+    stream = stream.pipe(plugins.plumber());
+  }
+
+  stream = stream.pipe(plugins.jscs())
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter(stylish));
+
+  if (dieOnError) {
+    stream = stream.pipe(plugins.jshint.reporter('fail'));
+  }
+
+  return stream;
 });
 
 gulp.task('js', ['js-quality'], function () {
@@ -28,11 +41,13 @@ gulp.task('sass', function () {
     .pipe(plugins.rubySass())
     .pipe(plugins.plumber())
     .pipe(plugins.autoprefixer())
-//		.pipe(plugins.minifyCss())
+//    .pipe(plugins.minifyCss())
     .pipe(gulp.dest('./demo/build'));
 });
 
-gulp.task('browser-sync', function () {
+gulp.task('default', ['js', 'sass'], function () {
+  dieOnError = false;
+
   browserSync.init([
     'demo/**/*.css',
     'demo/build/**/*.js',
@@ -49,9 +64,7 @@ gulp.task('browser-sync', function () {
       forms: false
     }
   });
-});
 
-gulp.task('default', ['js', 'sass', 'browser-sync'], function () {
   gulp.watch('sass/**/*.{sass,scss}', ['sass']);
 
   // Watching heidelberg file in case of npm link
