b831fc9f8d60e9c2101927b26c493bed51c2d056
Tom Cartwright <tecartwright@gmail.com>
2015-05-15 10:03:07 +0100

    WIP character swapping on monkey

--- src/js/steps/letters/init.js
+++ src/js/steps/letters/init.js
@@ -5,7 +5,8 @@ var $ = require('jquery');
 module.exports = function ($events) {
   return function (data) {
     var $letters = data.lettersElement.find('#letters');
-    var $spans = $letters.find('span:not(.special-char)');
+    var $spans = $letters.find('span');
+    var $charButtons = $letters.find('button')
 
     $events.on('letterChange', function (e, page) {
       var currentPage = Math.floor((page - 1) / 2);
@@ -29,6 +30,27 @@ module.exports = function ($events) {
       var charsBefore = $this.prevAll('.special-char').length;
       data.turnToPage($this.index() - charsBefore);
     });
+
+    $charButtons.on('click', function () {
+      var $buttonEl = $(this);
+      var character = $buttonEl.data('char');
+      var page = $buttonEl.data('page');
+
+      var selectedChar = $('#letters .letter-active').find('.selected-char');
+      selectedChar.removeClass('selected-char');
+      var $prevButton = selectedChar.find('button');
+      $prevButton
+        .attr('disabled', false)
+        .text('select')
+        .addClass('primary');
+
+      $buttonEl
+        .attr('disabled', true)
+        .removeClass('primary')
+        .text('selected');
+      $buttonEl.parent().addClass('selected-char')
+      data.swapPage(page, character);
+    })
 
     return data;
   };
