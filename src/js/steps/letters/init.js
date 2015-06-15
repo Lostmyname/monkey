6fb6b65a2e17ec07e12689966bc450349a0555d4
Tom Cartwright <tecartwright@gmail.com>
2015-06-15 13:13:13 +0100

    Set width of characters

--- src/js/steps/letters/init.js
+++ src/js/steps/letters/init.js
@@ -47,10 +47,19 @@ module.exports = function ($events, options) {
       data.turnToPage($this.index() - charsBefore);
     });
 
+    var calculatedWidth = 0;
+    $spans.each(function () {
+      calculatedWidth  += $(this).outerWidth(true);
+    });
+    calculatedWidth += 'px';
+    $letters.css({ width: calculatedWidth });
+
     if (isMobile && options.icons && options.animateName) {
-        $letters
-          .delay(animationDelay)
-          .animate({ marginLeft: 100 }, animationSpeed);
+      $letters
+        .delay(animationDelay)
+        .animate({ marginLeft: 100 }, animationSpeed);
+    } else if (isMobile) {
+      $letters.css({ marginLeft: 100 });
     }
 
     return data;
