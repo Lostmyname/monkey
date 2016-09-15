62a2c12ee05651691bdccc6f151164f3447fc8d1
Marc Thomas <hello@mrcthms.com>
2016-09-15 14:38:51 +0100

    Add raf polyfill

--- src/js/monkeys/desktop.js
+++ src/js/monkeys/desktop.js
@@ -5,6 +5,34 @@ var $ = require('jquery');
 var Heidelberg = require('heidelberg');
 var nums = require('nums');
 
+(function () {
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+      || window[vendors[x]+'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function (callback) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function () {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
+
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function (id) {
+      clearTimeout(id);
+    };
+  }
+})();
+
 var desktop = module.exports = {};
 
 desktop.calculateSize = function () {
